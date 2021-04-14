c7f23fff3b339f4bdaa7958cccf07e260b5a5a3f
Louis Liao <39911137+a401120174@users.noreply.github.com>
2021-04-14 23:39:51 +0800

    change to hash mode

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:a401120174/vue-payment.git master:gh-pages
+git push -f https://github.com/a401120174/vue-payment.git master:gh-pages
 
 cd -
